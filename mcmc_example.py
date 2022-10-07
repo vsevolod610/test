9fb22e5e4b1a3ca7f497894d75d1aa194f760cc1
Vsevolod Shenyavskiy <Vsewolod.610@gmail.com>
2022-10-07 12:54:16 +0300

    divide mcmc

---
+++ mcmc_example.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+MCMC analyze example "Sin"
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from chainconsumer import ChainConsumer
+
+from mcmc_kern import mcmc_kern, pic_chain, pic_fit
+
+
+def example_model(a, b, c, x0, z, x):
+    return a * np.sin(c * (x - x0) ** z) + b 
+
+
+if __name__ == "__main__":
+    # model
+    params_names = ['a', 'b', 'c', 'x0']
+    const_names = ['z']
+    model = example_model
+    ndim = len(params_names)
+
+    prior_data = dict()
+    prior_data['box'] = {params_names.index('a'): [2, 8],
+                         params_names.index('b'): [1, 4]}
+    prior_data['gauss'] = {params_names.index('b'): [2, 0.1, 0.1]}
+    prior_data['const'] = {const_names.index('z'): 1.0}
+
+    const = list(prior_data['const'].values())
+
+    # data
+    params_true = [5, 2, 1, 0]
+    N = 100
+    x = np.linspace(0, 10, N)
+    y = model(*params_true, *const, x) + np.random.normal(0, 2, N)
+    yerr = np.abs(np.random.normal(2, 0.5, (N, 2)))
+
+    # settings
+    params_try = np.array([5, 2, 1, 0])
+    params_sigma = np.array([2, 3, 0.1, 0.1])
+    init = np.array([params_try, params_sigma]).T
+
+    nwalkers = 100
+    nsteps = 200
+    amputete = int(0.5 * nsteps)
+
+
+    # mcmc realyze
+    sampler = mcmc_kern(model, nwalkers, nsteps, ndim, init, x, y, yerr, prior_data)
+
+    flat_sample = sampler.chain[:, amputete : , :].reshape((-1, ndim))
+    c = ChainConsumer()
+    c.add_chain(flat_sample, parameters=params_names)
+    summary = c.analysis.get_summary(parameters=params_names)
+    print("\nMCMC results:")
+    print(*[" {:>4}: {}".format(k, summary[k]) for k in summary.keys()], sep='\n')
+
+    # Pics
+    fig, ax = pic_chain(sampler, params_names)
+    fig = c.plotter.plot(display=False, legend=False, figsize=(6, 6))
+    fig, ax = pic_fit(sampler, model, x, y, yerr, prior_data)
+
+    plt.show()
+
--- mcmc_kern.py
+++ mcmc_kern.py
@@ -1,50 +1,47 @@
 # -*- coding: utf-8 -*-
 """
-MCMC
+MCMC kern
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import emcee
 from multiprocessing import Pool
-from chainconsumer import ChainConsumer
 
 
-def log_prior_box(v, vleft, vright):
-    if vleft < v < vright:
-        return 0.0
-    return -np.inf
+# MCMC
 
-
-def log_prior_gauss(v, mean, sigma_p, sigma_m):
-    sigma = sigma_p if (v - mean) else sigma_m
-    return - 0.5 * (v - mean) ** 2 / sigma ** 2
-
-
-def prior(params, prior_data):
+def prior_func(params, prior_data):
     if not prior_data:
         return 0
     prior_value = 0
 
-    for p_k in list(prior_data['box']):
-        left, right = prior_data['box'][p_k]
-        prior_value += log_prior_box(params[p_k], left, right)
+    # box prior
+    for k in list(prior_data['box']):
+        left, right = prior_data['box'][k]
+        if not (left < params[k] < right):
+            prior_value += -np.inf
 
-    for p_k in list(prior_data['gauss']):
-        mu, sigmap, sigmam = prior_data['gauss'][p_k]
-        prior_value += log_prior_gauss(params[p_k], mu, sigmap, sigmam)
+    # gauss prior
+    for k in list(prior_data['gauss']):
+        mu, sigma_p, sigma_m = prior_data['gauss'][k]
+        sigma = sigma_p if (params[k] - mu) else sigma_m
+        prior_value += -0.5 * (params[k] - mu)**2 / sigma**2
 
     return prior_value 
 
 
 def log_probability(params, model, x, y, yerr=0, prior_data=0):
+
+    # priors
     if not prior_data:
         prior_value = 0
     else:
-        prior_value = prior(params, prior_data)
+        prior_value = prior_func(params, prior_data)
         if not np.isfinite(prior_value):
             return -np.inf
 
+    # model
     const = []
     if type(prior_data) == dict:
         if 'const' in prior_data:
@@ -52,6 +49,7 @@ def log_probability(params, model, x, y, yerr=0, prior_data=0):
 
     m = model(*params, *const, x)
 
+    # sigma
     N = len(y)
     if np.shape(yerr) == ():
         sigma2 = np.ones(N)
@@ -62,6 +60,7 @@ def log_probability(params, model, x, y, yerr=0, prior_data=0):
         for i in range(N):
             sigma2[i] = (yerr[i,1] if m[i] > y[i] else yerr[i, 0]) ** 2
 
+    # lp
     lp_value = -0.5 * np.sum([(y[i] - m[i]) ** 2 / sigma2[i] for i in range(N)])
     lp_value += prior_value
     return lp_value
@@ -72,8 +71,8 @@ def mcmc_kern(model, nwalkers, nsteps, ndim, init, x, y, yerr=0, prior_data=0):
 
     # check init posintion in prior-box
     for k, param in enumerate(pos):
-        while not np.isfinite(prior(param, prior_data)):
-            param = params_try + params_sigma * np.random.rand(ndim)
+        while not np.isfinite(prior_func(param, prior_data)):
+            param = init[:, 0] + init[:, 1] * np.random.rand(ndim)
         pos[k] = param
 
     # mcmc mechanism
@@ -85,7 +84,7 @@ def mcmc_kern(model, nwalkers, nsteps, ndim, init, x, y, yerr=0, prior_data=0):
     return sampler
 
 
-# Pic's
+# Pics
 
 def pic_chain(sampler, params_names):
     samples = sampler.get_chain()
@@ -94,12 +93,13 @@ def pic_chain(sampler, params_names):
     fig, ax = plt.subplots(nrows=ndim, figsize=(10, 7), sharex=True)
     samples = sampler.get_chain()
 
+    # plot(chain)
     for i, row in enumerate(ax, start=0):
         row.plot(samples[:, :, i], "k", alpha=0.3)
         row.set_ylabel(params_names[i], fontsize=12)
 
     row.set_xlabel(r'steps', fontsize=12)
-    return fig
+    return fig, ax
 
 
 def pic_fit(sampler, model, x, y, yerr, prior_data):
@@ -114,70 +114,20 @@ def pic_fit(sampler, model, x, y, yerr, prior_data):
         if 'const' in prior_data:
             const = list(prior_data['const'].values())
 
+    # plot(set of fit)
     for w in sample_last:
         ax.plot(x, model(*w, *const, x), 'b', alpha=0.09)
 
+    # plot(data)
     if np.shape(yerr) == ():
         ax.plot(x, y, '.k', alpha=0.5, label='data')
     else:
         ax.errorbar(x, y, yerr.T, label='data',
                 capsize=3.5, mew=1.5, fmt='.k', alpha=0.5)
 
+    # plot(best fit)
     ax.plot(x, model(*params_chi, *const, x), 'r', label='best fit')
 
     ax.legend(frameon=False)
-    return fig
+    return fig, ax
 
-
-def example_model(a, b, c, x0, z, x):
-    return a * np.sin(c * (x - x0)) + b - z
-
-
-if __name__ == "__main__":
-    # model
-    params_names = ['a', 'b', 'c', 'x0']
-    const_names = ['z']
-    model = example_model
-
-    prior_data = dict()
-    prior_data['box'] = {params_names.index('a'): [2, 8],
-                         params_names.index('b'): [1, 4]}
-    prior_data['gauss'] = {params_names.index('b'): [2, 0.1, 0.1]}
-    prior_data['const'] = {const_names.index('z'): 0.0}
-    const = list(prior_data['const'].values())
-
-    # data
-    params_true = [5, 2, 1, 0]
-    N = 100
-    x = np.linspace(0, 10, N)
-    y = model(*params_true, *const, x) + np.random.normal(0, 2, N)
-    yerr = np.abs(np.random.normal(2, 0.5, (N, 2)))
-
-    # settings
-    ndim = len(params_names)
-
-    params_try = np.array([5, 2, 1, 0])
-    params_sigma = np.array([2, 3, 0.1, 0.1])
-    init = np.array([params_try, params_sigma]).T
-
-    nwalkers = 100
-    nsteps = 200
-    amputete = int(0.5 * nsteps)
-
-    # mcmc
-    sampler = mcmc_kern(model, nwalkers, nsteps, ndim, init, x, y, yerr, prior_data)
-
-    flat_sample = sampler.chain[:, amputete : , :].reshape((-1, ndim))
-    c = ChainConsumer()
-    c.add_chain(flat_sample, parameters=params_names)
-
-    summary = c.analysis.get_summary(parameters=params_names)
-    print("\nMCMC results:")
-    print(*[" {:>4}: {}".format(k, summary[k]) for k in summary.keys()], sep='\n')
-
-    # Pics
-    fig = pic_chain(sampler, params_names)
-    fig = c.plotter.plot(display=False, legend=False, figsize=(6, 6))
-    fig = pic_fit(sampler, model, x, y, yerr, prior_data)
-
-    plt.show()
