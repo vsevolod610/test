318462d287fdb609c4653c330556dee7c7889edb
Vsevolod Shenyavskiy <Vsewolod.610@gmail.com>
2023-02-26 13:56:58 +0300

    add minimize and integrate in math module

--- math/solve.py
+++ math/solve.py
@@ -10,3 +10,30 @@ a = 3 / np.pi
 f = lambda x: np.sin(x)/x - a
 
 print(fsolve(f, 2))
+
+
+"""
+Math: find minimum
+"""
+
+import numpy as np
+from scipy.optimize import minimize
+
+f = lambda x: -1 * x * np.exp(-x)
+x_start = 1
+soln = minimize(f, x_start)
+print(soln.x)
+
+
+"""
+Math: numerical integrate
+"""
+
+import numpy as np
+from scipy import integrate
+
+f = lambda x: np.exp(-x ** 2)
+x_start = -np.Inf
+x_stop = np.Inf
+solv = integrate.quad(f, x_start, x_stop)
+print(solv)
